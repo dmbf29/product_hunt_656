7893bb1aa2f6e5376157fc687356f75fef02153e
Douglas Berkley <douglasmberkley@gmail.com>
2021-08-27 10:03:40 +0900

    Added github action - hello world

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: 'rhys', last_name: 'malyon')
+    user.full_name == "Rhys Malyon"
+    # Mini Test way:
+    assert_equal "Rhys Malyon", user.full_name
+    # Rspec way:
+    # expect(user.full_name).to eq("Rhys Malyon")
+  end
 end
